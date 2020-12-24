957436aefebaf94f9f7c71b22756ed4fc00f12c2
sumanth445 <73520378+sumanth445@users.noreply.github.com>
2020-12-24 19:45:15 +0530

    Update g_config.py

--- tobrot/g_config.py
+++ tobrot/g_config.py
@@ -5,7 +5,7 @@ class Config(Config):
 	TG_BOT_TOKEN= "1442814217:AAEovdATuh5zbjNfeWisXQ-3jD28IKhvrK4"
 	APP_ID = 2865840
 	API_HASH = "610823c1999519c8261b215dbb372eee"
-	OWNER_ID = "126110134"
+	OWNER_ID = "1126110134"
 	AUTH_CHANNEL = [-1001470368690]
 	DESTINATION_FOLDER = "HK ROBOT" #Name of your folder read readme
 	RCLONE_CONFIG = """type = drive\nscope = drive\ntoken = {"access_token":"ya29.a0AfH6SMAJA9D504m5oQrShJzrnATfrWS--2qa7_Ia2cV6Lwo4VqNbO2XhAsHkE3TlnoxzsQQ1bT3TgOlA8ysG0LRgcbl1FXz3jwnJuW6ZYNC5bmIq5dODFLlipo7PMnAYmNuHDk4Z-x1KPtwI5cRSNnftrpLqG4bG0qbCrdTciYA","token_type":"Bearer","refresh_token":"1//0gQ7W5XLBdHI6CgYIARAAGBASNwF-L9IrZCQ5l0xZA9KkqD-9CTZFeR65IWR-SrbXCmxVi-CnNrK8dC4kn7AxaG2r1KeBYwmoVeI","expiry":"2020-12-21T15:19:36.015520994Z"}"""
